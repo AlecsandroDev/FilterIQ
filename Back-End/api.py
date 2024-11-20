96e7cf910be0a1e2ddb649a39460f9e2876b7b21
Alecsandro_Dev <107969775+AlecsandroDev@users.noreply.github.com>
2024-11-19 21:17:53 -0300

    Update api.py

--- Back-End/api.py
+++ Back-End/api.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
+from os import getenv
 
 app = Flask(__name__)
 CORS(app)
@@ -7,4 +9,5 @@ CORS(app)
 from routes import *
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    load_dotenv()
+    app.run(debug=True, port=getenv("port"))
